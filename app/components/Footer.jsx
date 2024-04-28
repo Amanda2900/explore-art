acaf5020dd48e46053d614df83c55d075d578291
Amanda DiNoto <amanda.dinoto@gmail.com>
2024-04-27 22:27:38 -0400

    add footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-right my-5 mr-5 text-gray-900">
+      <p> &copy; Amanda DiNoto 2024</p>
+    </footer>
+  );
+}
--- app/search/page.jsx
+++ app/search/page.jsx
@@ -15,8 +15,8 @@ export default function Search() {
 
   return (
     <main> 
-      <div className="text-center mx-10 mb-5 mt-10">
-      <h1 className="font-bold text-2xl mb-5 "> Art Institute of Chicago Search</h1>
+      <div className="text-center mx-10 mt-10">
+      <h1 className="font-bold text-2xl mb-5 "> Search the Art Institute of Chicago</h1>
         <SearchForm onSearchSubmit={onSearchSubmit}/>
         <div className="bg-top h-screen bg-no-repeat bg-opacity-75" style={{backgroundImage: "url(/Art_Institute_of_Chicago_logo.png)"}}>
             <ImageList data={data} />
